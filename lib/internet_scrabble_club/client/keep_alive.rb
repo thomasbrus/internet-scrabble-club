d80863b0907d48eb36ebabe824c1809e03f2539a
Thomas Brus <thomas.brus@me.com>
2014-04-08 13:35:07 +0200

    Changed keep alive interval

--- lib/internet_scrabble_club/client/keep_alive.rb
+++ lib/internet_scrabble_club/client/keep_alive.rb
@@ -3,11 +3,11 @@ module InternetScrabbleClub
 
     module KeepAlive
       def initialize(*args, &block)
-        every(3) { keep_alive } and super
+        every(30) { keep_alive } and super
       end
 
       def keep_alive
-        send_message(:alive, 7, current_time_with_milliseconds, memory_usage, t1, t2, 0)
+        # send_message(:alive, 7, current_time_with_milliseconds, memory_usage, t1, t2, 0)
       end
     end
 
