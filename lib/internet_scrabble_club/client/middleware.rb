fcb46c1a62447d4d144e965c56df8f4b2c97974b
Thomas Brus <thomas.brus@me.com>
2014-04-12 17:21:32 +0200

    Removed empty middleware module

--- lib/internet_scrabble_club/client/middleware.rb
+++ lib/internet_scrabble_club/client/middleware.rb
@@ -1,15 +1,4 @@
-require 'parslet'
-
 require_relative 'middleware/emit'
 require_relative 'middleware/parse'
 require_relative 'middleware/read'
 require_relative 'middleware/transform'
-
-module InternetScrabbleClub
-  class Client
-
-    module Middleware
-    end
-
-  end
-end
