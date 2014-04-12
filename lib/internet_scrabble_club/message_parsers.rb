3d420ff71a50ae08a2caaa354025467618aad264
Thomas Brus <thomas.brus@me.com>
2014-04-12 17:20:50 +0200

    Updated require paths in message_parsers.rb

--- lib/internet_scrabble_club/message_parsers.rb
+++ lib/internet_scrabble_club/message_parsers.rb
@@ -1,13 +1,8 @@
-require_relative 'message_parsers/response/history'
-require_relative 'message_parsers/response/login'
-require_relative 'message_parsers/response/ping'
-require_relative 'message_parsers/response/seek'
-require_relative 'message_parsers/response/unseek'
-require_relative 'message_parsers/response/who'
+require_relative 'message_parsers/examine/history'
+require_relative 'message_parsers/who/list'
 
-module InternetScrabbleClub
-
-  module MessageParsers
-  end
-
-end
+require_relative 'message_parsers/history'
+require_relative 'message_parsers/login'
+require_relative 'message_parsers/ping'
+require_relative 'message_parsers/seek'
+require_relative 'message_parsers/unseek'
