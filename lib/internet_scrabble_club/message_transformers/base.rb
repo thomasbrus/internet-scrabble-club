4ca80bb4e028c215c4a5abda4c2801cdee0bebe6
Thomas Brus <thomas.brus@me.com>
2014-04-13 16:48:25 +0200

    Added rules for converting primitives

--- lib/internet_scrabble_club/message_transformers/base.rb
+++ lib/internet_scrabble_club/message_transformers/base.rb
@@ -1,3 +1,4 @@
+require 'date'
 require 'parslet'
 require 'descendants_tracker'
 
@@ -6,6 +7,15 @@ module InternetScrabbleClub
 
     class Base < Parslet::Transform
       extend DescendantsTracker
+
+      rule(int: simple(:int)) { Integer(int) }
+      rule(word: simple(:word)) { word.to_s }
+      rule(date: simple(:date)) { Date.parse(date.to_s) }
+
+      rule(null: simple(:null)) { 0 }
+      rule(tiles: simple(:tiles)) { tiles.to_s }
+
+      rule(dashes: simple(:dashes)) { nil }
 
       def apply(tree, context = nil)
         descendent_transformers.reduce(super) do |tree, transformer|
