a07d5d30ace16b6efd0143127305dfc725ec7295
Thomas Brus <thomas.brus@me.com>
2014-04-13 18:05:29 +0200

    Added examine/history transformer

--- lib/internet_scrabble_club/message_transformers/examine/history.rb
+++ lib/internet_scrabble_club/message_transformers/examine/history.rb
@@ -1,30 +1,32 @@
 require 'ostruct'
-require_relative '../examine'
+require_relative '../base'
 
 module InternetScrabbleClub
   class MessageTransformers
+    module Examine
 
-    class Examine::History < Examine
-      rule(date: simple(:date), dictionary_code: simple(:dictionary_code),
-        first_player: subtree(:first_player), second_player: subtree(:second_player)
-      ) do
-        { date: date, dictionary_code: dictionary_code,
-          first_player: first_player, second_player: second_player
-        }
+      class History < Base
+        rule(date: simple(:date), dictionary_code: simple(:dictionary_code),
+          first_player: subtree(:first_player), second_player: subtree(:second_player)
+        ) do
+          { date: date, dictionary_code: dictionary_code,
+            first_player: first_player, second_player: second_player
+          }
+        end
+
+        rule(setup: subtree(:setup), plays: subtree(:plays)) do
+          OpenStruct.new(setup: setup, plays: plays.map { |play| OpenStruct.new(play) })
+        end
+
+        rule(horizontal: { column: simple(:column), row: simple(:row) }) do
+          OpenStruct.new(direction: :horizontal, column: column, row: row)
+        end
+
+        rule(vertical: { column: simple(:column), row: simple(:row) }) do
+          OpenStruct.new(direction: :vertical, column: column, row: row)
+        end
       end
 
-      rule(setup: subtree(:setup), plays: subtree(:plays)) do
-        OpenStruct.new(setup: setup, plays: plays.map { |play| OpenStruct.new(play) })
-      end
-
-      rule(horizontal: { column: simple(:column), row: simple(:row) }) do
-        OpenStruct.new(direction: :horizontal, column: column, row: row)
-      end
-
-      rule(vertical: { column: simple(:column), row: simple(:row) }) do
-        OpenStruct.new(direction: :vertical, column: column, row: row)
-      end
     end
-
   end
 end
