204c2953f61615daa2680c621f5534a880bd8b8e
Thomas Brus <thomas.brus@me.com>
2014-04-13 18:52:44 +0200

    Added parent message parsers for completeness

--- lib/internet_scrabble_club/message_parsers.rb
+++ lib/internet_scrabble_club/message_parsers.rb
@@ -1,8 +1,15 @@
+require_relative 'message_parsers/examine'
 require_relative 'message_parsers/examine/history'
+
+require_relative 'message_parsers/who'
 require_relative 'message_parsers/who/list'
 require_relative 'message_parsers/who/move'
+
 require_relative 'message_parsers/history'
 require_relative 'message_parsers/login'
+
+require_relative 'message_parsers/ping'
 require_relative 'message_parsers/ping/reply'
+
 require_relative 'message_parsers/seek'
 require_relative 'message_parsers/unseek'
